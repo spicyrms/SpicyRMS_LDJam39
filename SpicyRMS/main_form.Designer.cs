07aa202ba5fdb8dc86350ff50346e06ffc66b7a0
SpicyRMS <spicyrms@gmail.com>
2017-07-29 19:18:41 -0400

    Added some game assets and setup initial view

--- SpicyRMS/main_form.Designer.cs
+++ SpicyRMS/main_form.Designer.cs
@@ -28,21 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main_form));
+            this.background_picture_box = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.background_picture_box)).BeginInit();
             this.SuspendLayout();
+            // 
+            // background_picture_box
+            // 
+            this.background_picture_box.Image = ((System.Drawing.Image)(resources.GetObject("background_picture_box.Image")));
+            this.background_picture_box.Location = new System.Drawing.Point(0, 0);
+            this.background_picture_box.Name = "background_picture_box";
+            this.background_picture_box.Size = new System.Drawing.Size(1000, 650);
+            this.background_picture_box.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.background_picture_box.TabIndex = 0;
+            this.background_picture_box.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Location = new System.Drawing.Point(319, 266);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(356, 257);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(341, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(310, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Welcome to your terminal.";
             // 
             // main_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 711);
+            this.ClientSize = new System.Drawing.Size(1000, 650);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.background_picture_box);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "main_form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SpicyRMS";
+            this.Load += new System.EventHandler(this.main_form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.background_picture_box)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox background_picture_box;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- SpicyRMS/main_form.cs
+++ SpicyRMS/main_form.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,11 +14,26 @@ namespace SpicyRMS
 {
     public partial class main_form : Form
     {
+        private SoundPlayer music_player = new SoundPlayer();
+        
         public main_form()
         {
             InitializeComponent();
             Thread.Sleep(8000);
             splash_screen.splash_screen.dispose_form();
+        }
+
+        private void main_form_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.music_player.SoundLocation = @"D:\dev\game_jam\SpicyRMS\SpicyRMS\assets\audio\music\ending.wav";
+                this.music_player.PlayLooping();
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message, "Error playing audio");
+            }
         }
     }
 }
